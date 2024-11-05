71d7ea45fa7af0bc212964573063c0921423c806
Marc Scheibner <sinus.ms@gmx.de>
2024-11-05 01:18:39 +0100

    configure backend_root path, add navigation buttons

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "sessions#show"
+  get "/backend", to: "sessions#show", as: "backend_root"
 
   resources :users, only: [ :new, :create, :edit, :update, :show, :destroy, :index ]
 
