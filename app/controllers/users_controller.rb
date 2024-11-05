08b3fa8d23dde29ad56d217bf95156dd2d9a23d6
Marc Scheibner <sinus.ms@gmx.de>
2024-11-05 16:41:38 +0100

    add proper username validation, fix user#create error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
       return
     end
 
-    @user = User.new(user_params[:username])
+    @user = User.new(user_params)
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: "User was successfully created." }
