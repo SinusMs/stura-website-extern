a51e02aa89262836b22d15e8bc579e0170cac667
Marc Scheibner <sinus.ms@gmx.de>
2024-11-06 23:49:12 +0100

    customize mailer test and make some adjustments to mailer

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -2,10 +2,18 @@ require "test_helper"
 
 class ContactMailerTest < ActionMailer::TestCase
   test "contact" do
-    mail = ContactMailer.contact
-    assert_equal "Contact", mail.subject
-    assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = ContactMailer.with(name: contact_email_addresses(:stu).name, text: user_text, your_email: user_email).contact
+    assert_equal "Contact request by " + user_email, mail.subject
+    assert_equal [ contact_email_addresses(:stu).email_address ], mail.to
+    assert_equal [ user_email ], mail.from
+    puts mail.body.encoded
+    assert_match Regexp.new(user_text), mail.body.encoded
+  end
+
+  def user_email
+    "max@mustermann.de"
+  end
+  def user_text
+    "Hallo! K=C3=B6nnt ihr mir bei meinem Baf=C3=B6g-Antrag helfen?"
   end
 end
