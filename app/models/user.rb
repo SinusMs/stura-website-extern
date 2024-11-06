632afd7ce2ee875739ebf951ace7d4350c1a01b6
Marc Scheibner <sinus.ms@gmx.de>
2024-11-06 13:52:26 +0100

    fix faulty regex (again :P)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: 3 }, unless: Proc.new { |user| user.password.nil? }
-  validates :username, uniqueness: true, length: { minimum: 3 }, format: { with: /\A[0-9A-Za-z]\z/, message: "Use only numbers and letters for your Username!" }
+  validates :username, uniqueness: true, length: { minimum: 3 }, format: { with: /\A[0-9A-Za-z]*\z/, message: "Use only numbers and letters for your Username!" }
 end
