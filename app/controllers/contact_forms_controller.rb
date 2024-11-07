638cbad400a11a58280d1be0a0bf65e7eb734551
Marc Scheibner <sinus.ms@gmx.de>
2024-11-07 11:34:49 +0100

    small refactoring for contact form

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -3,7 +3,7 @@ class ContactFormsController < ApplicationController
     @contact_names = ContactEmailAddress.select("contact_email_addresses.name")
   end
   def post
-     ContactMailer.with(name: params[:name], text: params[:text], your_email: params[:your_email]).contact.deliver_now
+     ContactMailer.with(contact_email_address_id: params[:contact_email_address_id], text: params[:text], your_email: params[:your_email]).contact.deliver_now
      redirect_to request.referrer, notice: "Your Request has been Sent!"
   end
 end
