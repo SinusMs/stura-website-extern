7a74e915b0d53d27f9921d0515da86d90bcf0ef4
Marc Scheibner <sinus.ms@gmx.de>
2024-11-07 11:03:16 +0100

    adjustments to contact email addresses system test

--- test/system/contact_email_addresses_test.rb
+++ test/system/contact_email_addresses_test.rb
@@ -15,16 +15,15 @@ class ContactEmailAddressesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Contact email addresses"
   end
 
-  test "should create contact email address" do
+  test "should create contact email address with unique name" do
     visit contact_email_addresses_url
     click_on "New contact email address"
 
-    fill_in "Email address", with: @contact_email_address.email_address
-    fill_in "Name", with: @contact_email_address.name
+    fill_in "Email address", with: valid_contact_email_address.email_address
+    fill_in "Name", with: valid_contact_email_address.name
     click_on "Create Contact email address"
 
     assert_text "Contact email address was successfully created"
-    click_on "Back"
   end
 
   test "should update Contact email address" do
@@ -36,7 +35,6 @@ class ContactEmailAddressesTest < ApplicationSystemTestCase
     click_on "Update Contact email address"
 
     assert_text "Contact email address was successfully updated"
-    click_on "Back"
   end
 
   test "should destroy Contact email address" do
@@ -44,5 +42,9 @@ class ContactEmailAddressesTest < ApplicationSystemTestCase
     click_on "Destroy this contact email address", match: :first
 
     assert_text "Contact email address was successfully destroyed"
+  end
+
+  def valid_contact_email_address
+    ContactEmailAddress.new(name: "vaild name", email_address: "vaild@email.com")
   end
 end
