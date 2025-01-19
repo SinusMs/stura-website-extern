082d326b03418db09472fadf58f240ab327f2885
Marc Scheibner <m.scheibner@paindevs.com>
2025-01-19 12:31:44 +0100

    (WIP) toggle checkbox

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+function toggleRowVisibility() {
+    var hiddenRow = document.getElementById("hidden-row");
+
+    if (hiddenRow.style.display === "none") {
+      hiddenRow.style.display = "";
+    } else {
+      hiddenRow.style.display = "none";
+    }
+  }
