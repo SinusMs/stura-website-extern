f26785d1869c95b43e32d666c42a7733d1c2115e
Marc Scheibner <m.scheibner@paindevs.com>
2025-01-19 13:39:00 +0100

    working toggle

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-function toggleRowVisibility() {
-    var hiddenRow = document.getElementById("hidden-row");
-
-    if (hiddenRow.style.display === "none") {
-      hiddenRow.style.display = "";
-    } else {
-      hiddenRow.style.display = "none";
-    }
-  }
