8c69f33436cbcc5168a8c7fc74185131f544ceb6
Marc Scheibner <m.scheibner@paindevs.com>
2025-04-10 15:17:53 +0200

    adjust system tests

--- test/system/settings_test.rb
+++ test/system/settings_test.rb
@@ -3,6 +3,11 @@ require "application_system_test_case"
 class SettingsTest < ApplicationSystemTestCase
   setup do
     @settings = settings(:settings)
+    visit login_url
+    fill_in "username", with: "admin"
+    fill_in "password", with: "123"
+    click_on "login"
+    assert_text "Logged in: true"
   end
 
   test "should update Settings" do
