33fd7e96889d05a3716528450cf6d83ca7e39524
Marc Scheibner <sinus.ms@gmx.de>
2025-05-09 13:07:46 +0200

    contact email addresses validation

--- app/models/contact_email_address.rb
+++ app/models/contact_email_address.rb
@@ -1,4 +1,5 @@
 class ContactEmailAddress < ApplicationRecord
+  validates :name, :email_address, presence: true
   validates :name, uniqueness: true
   validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid" }, confirmation: true
 end
