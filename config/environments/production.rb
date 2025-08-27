eab9b9aa19d837a99fbac477343a127b795db6c4
Marc Scheibner <147995870+SinusMs@users.noreply.github.com>
2025-08-27 18:57:04 +0200

    Bugfix: Remove SMTP Authentication (#86)
    * remove smtp authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,11 +86,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV["SMTP_ADDRESS"],
     port: ENV["SMTP_PORT"],
-    domain: ENV["APP_HOST"],
-    user_name: ENV["SMTP_USER_NAME"],
-    password: ENV["SMTP_PASSWORD"],
-    authentication: :login,
-    enable_starttls_auto: true
+    domain: ENV["APP_HOST"]
   }
   config.action_mailer.default_url_options = { host: ENV["APP_HOST"] }
 
