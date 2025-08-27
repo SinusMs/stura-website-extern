9ef4a570c0ecc075a68e8d2d13f8c87f6ceb50b5
Marc Scheibner <sinus.ms@gmx.de>
2025-08-27 18:04:33 +0200

    remove authentication type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,6 @@ Rails.application.configure do
     address: ENV["SMTP_ADDRESS"],
     port: ENV["SMTP_PORT"],
     domain: ENV["APP_HOST"],
-    authentication: :none,
   }
   config.action_mailer.default_url_options = { host: ENV["APP_HOST"] }
 
