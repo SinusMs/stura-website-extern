6cb03f8009382740844f4f98d264371134bbca4c
Marc Scheibner <sinus.ms@gmx.de>
2025-08-30 13:39:02 +0200

    translate custom user model validation error messages to german

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :password, length: { minimum: ENV.fetch("MINIMUM_USER_PASSWORD_LENGTH", 3).to_i }, unless: Proc.new { |user| user.password.nil? }
-  validates :username, uniqueness: true, length: { minimum: 3 }, format: { with: /\A[0-9A-Za-z_\-.]*\z/, message: "Use only numbers and letters for your Username!" }
-  validates :email_address, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "invalid" }
+  validates :username, uniqueness: true, length: { minimum: 3 }, format: { with: /\A[0-9A-Za-z_\-.]*\z/, message: "darf nur Ziffern und Buchstaben enthalten" }
+  validates :email_address, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "ungültig" }
 
   has_one :reset_password_code, class_name: "ResetPasswordCode", dependent: :destroy
 end
