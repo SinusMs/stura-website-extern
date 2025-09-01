08502369585d6a671851a452cf77df65133c0e0b
Marc Scheibner <sinus.ms@gmx.de>
2025-09-01 13:55:08 +0200

    article categories controller tests

--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -4,5 +4,14 @@ class ArticleCategory < ApplicationRecord
 
   scope :enabled, -> { where(enabled: true) }
 
-  has_many :articles, dependent: :nullify
+  has_many :articles
+
+  before_destroy :ensure_no_articles
+  private
+  def ensure_no_articles
+    if articles.exists?
+      errors.add(:base, "Kategorie kann nicht gelöscht werden, solange noch Artikel zugeordnet sind. Alternativ kann die Kategorie deaktiviert werden.")
+      throw(:abort)
+    end
+  end
 end
