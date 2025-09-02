a3f8618e3782b266b8902d17acead45902b91b84
Marc Scheibner <sinus.ms@gmx.de>
2025-09-02 11:45:09 +0200

    cache ics calendar for 5 min

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,16 +4,22 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    uri = URI("https://www.stura.htw-dresden.de/events/@@ics_view")
-    response = Net::HTTP.get(uri)
-    @calendar = Icalendar::Calendar.parse(response).first
+    @calendar = fetch_calendar
   end
 
   # GET /events/1 or /events/1.json
   def show
-    uri = URI("https://www.stura.htw-dresden.de/events/@@ics_view")
-    response = Net::HTTP.get(uri)
-    ics_event = Icalendar::Calendar.parse(response).first.events.find { |e| e.uid == params[:id] }
+    calendar = fetch_calendar
+    ics_event = calendar.events.find { |e| e.uid == params[:id] }
     @event = Event.new(ics_event: ics_event)
+  end
+
+  private
+
+  def fetch_calendar
+    raw_ics = Rails.cache.fetch(:stura_ics_calendar, expires_in: 5.minutes) do
+      Net::HTTP.get(URI("https://www.stura.htw-dresden.de/events/@@ics_view"))
+    end
+    Icalendar::Calendar.parse(raw_ics).first
   end
 end
